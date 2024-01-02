e8b4c7ca71b263e8913dbdfb18bbf93475ac7c72
theendercore <theendercore@gmail.com>
2024-01-02 04:10:14 +0200

    - added mc Serializers

---
+++ src/main/kotlin/org/teamvoided/template/serializers/BlockPosSerializer.kt
@@ -0,0 +1,15 @@
+package org.teamvoided.template.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import net.minecraft.util.math.BlockPos
+
+class BlockPosSerializer : KSerializer<BlockPos> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("block_pos", PrimitiveKind.STRING)
+    override fun serialize(encoder: Encoder, value: BlockPos) =encoder.encodeLong(value.asLong())
+    override fun deserialize(decoder: Decoder): BlockPos = BlockPos.fromLong(decoder.decodeLong())
+}
---
+++ src/main/kotlin/org/teamvoided/template/serializers/ChunkPosSerializer.kt
@@ -0,0 +1,15 @@
+package org.teamvoided.template.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import net.minecraft.util.math.ChunkPos
+
+class ChunkPosSerializer : KSerializer<ChunkPos> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("chunk_pos", PrimitiveKind.STRING)
+    override fun serialize(encoder: Encoder, value: ChunkPos) =encoder.encodeLong(value.toLong())
+    override fun deserialize(decoder: Decoder): ChunkPos = ChunkPos(decoder.decodeLong())
+}
